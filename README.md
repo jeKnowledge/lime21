764e95fe8d3c575485c88743af8990e601c94e32
dbgfribeiro <dribeiro641@gmail.com>
2021-04-14 19:50:56 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# lime21
-Lime Conference 2021 Website
+# Lime_conf
+Lime Conference Website
+
+2019 - Design made by Daniel Santos and implemented by Miguel Vasconcelos
+2020 - Design made by Joana Sousa and Daniel Santos and implemented by Joana Sousa and Miguel Vasconcelos
---
+++ assets/javascript/animation.js
@@ -0,0 +1,23 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('orador_1'),
+    renderer: 'svg',
+    loop:true,
+    autoplay:true,
+    path: 'orador1.json'
+});
+
+var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('orador_2'),
+    renderer: 'svg',
+    loop:true,
+    autoplay:true,
+    path: 'orador2.json'
+});
+
+var animation2_1 = bodymovin.loadAnimation({
+    container: document.getElementById('orador_2_1'),
+    renderer: 'svg',
+    loop:true,
+    autoplay:true,
+    path: 'orador2.json'
+});
